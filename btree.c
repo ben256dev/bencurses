df79b1dcff9b802a67f63ad892589180c4bb86d0
ben256dev <benjamin@ben256.com>
2024-08-13 20:25:31 +0900

    better traversal code

--- btree.c
+++ btree.c
@@ -7,10 +7,11 @@
 #include <assert.h>
 
 typedef uint8_t bnode;
-#define BNODE_IS_HORIZONTAL_BIT                        1 << 7
-#define BNODE_WINDOW                                        1
-#define BNODE_FRACTION_BITS      BNODE_IS_HORIZONTAL_BIT  - 1
-#define BNODE_CENTER_BITS        BNODE_IS_HORIZONTAL_BIT >> 1
+#define BNODE_IS_HORIZONTAL_BIT                   1 << 7
+#define BNODE_VISITATION_BIT                      1 << 6
+#define BNODE_WINDOW                                   1
+#define BNODE_FRACTION_BITS     BNODE_VISITATION_BIT - 1
+#define BNODE_CENTER_BITS       BNODE_FRACTION_BITS >> 1
 
 typedef uint8_t bwin;
 #define BNODE_IS_DIRTY_BIT 1 << 7
@@ -18,10 +19,28 @@ typedef uint8_t bwin;
 
 #define LC(x)         x << 1
 #define RC(x)        (x << 1) + 1
-#define IS_SISTER(x)  x & 1
-#define SISTER(x)     x + 1
+#define IS_SISTER(x)  x &  1
+#define SIBLING(x)    x ^  1
 #define PARENT(x)     x >> 1
-#define AUNT(x)      (x >> 1) + 1
+#define CHILD(x)      x << 1
+
+typedef struct bregion
+{
+	int rows, cols;
+} bregion;
+
+int rows, cols;
+bregion region;
+
+/*
+void node_size_eval(bnode node)
+{
+	switch 
+	{
+		case IS_SISTER()
+	}
+}
+*/
 
 int main(void)
 {
@@ -31,7 +50,6 @@ int main(void)
     	noecho();
     	keypad(stdscr, TRUE);
 
-    	int rows, cols;
     	getmaxyx(stdscr, rows, cols);
 
 	void* bencurses_alloc_ptr = calloc( 255, (sizeof(WINDOW*) + sizeof(bnode) + sizeof(bwin)) );
@@ -43,28 +61,50 @@ int main(void)
 	node_array[cur] = BNODE_WINDOW;
 	flag_array[cur] = BNODE_IS_DIRTY_BIT;
 	nwin_array[cur] = newwin(rows, cols, 0, 0);
+	box(nwin_array[cur], 0, 0);
 
 	int c;
 	while ( ( c = wgetch(nwin_array[1]) ) != 'q')
 	{
 		uint8_t n = 1;
 TRAVERSE_TREE:
-		if (n | 128 || node_array[n] == BNODE_WINDOW)
+		if (n == 1 && node_array[n] & BNODE_VISITATION_BIT)
 		{
-			if (IS_SISTER(n))
-			{
-				switch (n) 
-				{
-					case 1: case 2: case 4: case 8: case 16: case 32: case 64: case 128:
-						goto FINISH_TREE;
-						break;
-					default:
-						n = AUNT(n);
-				}
-			}
-			n = SISTER(n);
-			goto TRAVERSE_TREE;
+			node_array[n] ^= BNODE_VISITATION_BIT;
+			goto FINISH_TREE;
 		}
+		
+		if (node_array[n] == BNODE_WINDOW)
+		{
+			if (flag_array[n] | BNODE_IS_DIRTY_BIT)
+			{
+				wrefresh(nwin_array[n]);
+				flag_array[n] ^ BNODE_IS_DIRTY_BIT;
+			}
+
+			node_array[n] |= BNODE_VISITATION_BIT;
+		}
+
+		if (IS_SISTER(n))
+		{
+			if (node_array[n] & BNODE_VISITATION_BIT && node_array[n-1] & BNODE_VISITATION_BIT)
+			{
+				node_array[n]         ^= BNODE_VISITATION_BIT;
+				node_array[n-1]       ^= BNODE_VISITATION_BIT;
+				node_array[PARENT(n)] |= BNODE_VISITATION_BIT;
+				n = PARENT(n);
+			}
+		}
+		else
+		{
+			if (node_array[n] & BNODE_VISITATION_BIT)
+				n = SIBLING(n);
+			else
+				n = CHILD(n);
+
+		}
+		printf("%u\n", n);
+		goto TRAVERSE_TREE;
 FINISH_TREE:
 		switch (c)
 		{
